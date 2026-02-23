0823df9f4e9290cfd58218fabb7e8e306512f302
Dharmik <dharmik.victasoft@gmail.com>
2026-02-23 19:10:03 +0530

    Some Changes

--- Frontend/vite.config.js
+++ Frontend/vite.config.js
@@ -9,7 +9,7 @@ export default defineConfig({
         target: "http://127.0.0.1:5000",
         changeOrigin: true,
       },
-      allowedHosts: ["localhost", ".trycloudflare.com"],
+      allowedHosts: ["food-analyzer-1-kooe.onrender.com"],
     },
     host: true,
     port: 5173,
